745abdb80460a7dcca64d8a2a1e02273a80ebb00
arasht52 <212458577+arasht52@users.noreply.github.com>
2026-06-17 13:09:52 +0200

    Add favorites service

--- src/services/favoritesService.js
+++ src/services/favoritesService.js
@@ -1,1 +1,47 @@
-// TODO
+import { getItem, setItem } from "../utils/storage";
+import { STORAGE_KEYS } from "../constants/storageKeys";
+
+export const getFavorites = () => {
+  return getItem(STORAGE_KEYS.FAVORITES, []);
+};
+
+export const isFavorite = (movieId) => {
+  const favorites = getFavorites();
+  return favorites.some((item) => item.movieId === movieId);
+};
+
+export const addFavorite = (movie) => {
+  const favorites = getFavorites();
+
+  if (isFavorite(movie.id)) {
+    return favorites;
+  }
+
+  const nextFavorites = [
+    ...favorites,
+    {
+      movieId: movie.id,
+      title: movie.title,
+      savedAt: new Date().toISOString(),
+    },
+  ];
+
+  setItem(STORAGE_KEYS.FAVORITES, nextFavorites);
+  return nextFavorites;
+};
+
+export const removeFavorite = (movieId) => {
+  const favorites = getFavorites();
+  const nextFavorites = favorites.filter((item) => item.movieId !== movieId);
+
+  setItem(STORAGE_KEYS.FAVORITES, nextFavorites);
+  return nextFavorites;
+};
+
+export const toggleFavorite = (movie) => {
+  if (isFavorite(movie.id)) {
+    return removeFavorite(movie.id);
+  }
+
+  return addFavorite(movie);
+};
