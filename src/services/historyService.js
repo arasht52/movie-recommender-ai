619dcf9e153987742a0fc0ca5e11722a3a1e872c
arasht52 <212458577+arasht52@users.noreply.github.com>
2026-06-17 13:11:39 +0200

    Add history service

--- src/services/historyService.js
+++ src/services/historyService.js
@@ -1,1 +1,60 @@
-// TODO
+// import { getItem, setItem } from "../utils/storage";
+import { STORAGE_KEYS } from "../constants/storageKeys";
+
+export const getRecommendationHistory = () => {
+  return getItem(STORAGE_KEYS.RECOMMENDATION_HISTORY, []);
+};
+
+export const addRecommendationSession = ({
+  sessionId,
+  userVectorSnapshot,
+  results = [],
+}) => {
+  const history = getRecommendationHistory();
+
+  const nextHistory = [
+    ...history,
+    {
+      sessionId,
+      date: new Date().toISOString(),
+      userVectorSnapshot,
+      results: results.map((item) => ({
+        movieId: item.movie?.id,
+        score: item.score,
+        matchReasons: item.matchReasons || [],
+        warnings: item.warnings || [],
+      })),
+    },
+  ];
+
+  setItem(STORAGE_KEYS.RECOMMENDATION_HISTORY, nextHistory);
+  return nextHistory;
+};
+
+export const clearRecommendationHistory = () => {
+  setItem(STORAGE_KEYS.RECOMMENDATION_HISTORY, []);
+  return [];
+};
+
+export const getQuizState = () => {
+  return getItem(STORAGE_KEYS.QUIZ_STATE, {
+    step: "mood",
+    moodAnswers: [],
+    personalityAnswers: [],
+  });
+};
+
+export const saveQuizState = (quizState) => {
+  return setItem(STORAGE_KEYS.QUIZ_STATE, quizState);
+};
+
+export const clearQuizState = () => {
+  const emptyState = {
+    step: "mood",
+    moodAnswers: [],
+    personalityAnswers: [],
+  };
+
+  setItem(STORAGE_KEYS.QUIZ_STATE, emptyState);
+  return emptyState;
+};
