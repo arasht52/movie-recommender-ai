ba5aadce389c2c41cc72a56d4bfb94f97b699154
arasht52 <212458577+arasht52@users.noreply.github.com>
2026-06-17 13:23:45 +0200

    Add recommendation service

--- src/services/recommendationService.js
+++ src/services/recommendationService.js
@@ -1,1 +1,30 @@
-// TODO
+import { recommend } from "../engine/recommend";
+import { addRecommendationSession } from "./historyService";
+
+const createSessionId = () => {
+  if (typeof crypto !== "undefined" && crypto.randomUUID) {
+    return crypto.randomUUID();
+  }
+
+  return `session_${Date.now()}_${Math.random().toString(36).slice(2)}`;
+};
+
+export const getRecommendations = ({
+  movies = [],
+  userPreferences = {},
+  limit = 3,
+}) => {
+  const results = recommend({
+    movies,
+    userPreferences,
+    limit,
+  });
+
+  addRecommendationSession({
+    sessionId: createSessionId(),
+    userVectorSnapshot: userPreferences,
+    results,
+  });
+
+  return results;
+};
