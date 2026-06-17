664293d00d993a003129c3637a5a89454d485100
arasht52 <212458577+arasht52@users.noreply.github.com>
2026-06-17 13:12:22 +0200

    Add settings service

--- src/services/settingsService.js
+++ src/services/settingsService.js
@@ -1,1 +1,36 @@
-// TODO
+import { getItem, setItem } from "../utils/storage";
+import { STORAGE_KEYS } from "../constants/storageKeys";
+import { SCORING_WEIGHTS } from "../constants/scoringWeights";
+
+export const DEFAULT_SETTINGS = {
+  safetyFilters: {
+    avoidViolence: false,
+    avoidFear: false,
+    avoidHeavySadness: false,
+    avoidTriggeringThemes: true,
+  },
+  preferredTypes: ["movie", "series"],
+  preferredRegions: [],
+  preferredLanguages: [],
+  weightOverrides: SCORING_WEIGHTS,
+};
+
+export const getSettings = () => {
+  return getItem(STORAGE_KEYS.SETTINGS, DEFAULT_SETTINGS);
+};
+
+export const saveSettings = (settings) => {
+  return setItem(STORAGE_KEYS.SETTINGS, {
+    ...DEFAULT_SETTINGS,
+    ...settings,
+    safetyFilters: {
+      ...DEFAULT_SETTINGS.safetyFilters,
+      ...(settings?.safetyFilters || {}),
+    },
+  });
+};
+
+export const resetSettings = () => {
+  setItem(STORAGE_KEYS.SETTINGS, DEFAULT_SETTINGS);
+  return DEFAULT_SETTINGS;
+};
