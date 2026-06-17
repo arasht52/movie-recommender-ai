6649d9a1b6d14b6960a08d3c36a4bcea13f6181d
arasht52 <212458577+arasht52@users.noreply.github.com>
2026-06-17 13:09:11 +0200

    Add profile service

--- src/services/profileService.js
+++ src/services/profileService.js
@@ -1,1 +1,20 @@
-// TODO
+import { getItem, setItem } from "../utils/storage";
+
+const PROFILE_KEY = "userProfile";
+
+export const getProfile = () => {
+  return getItem(PROFILE_KEY, {
+    favoriteGenres: [],
+    favoriteActors: [],
+    favoriteDirectors: [],
+    favoriteCountries: [],
+    preferredLanguages: [],
+    preferredDecades: [],
+    favoriteCategories: [],
+    mood: null
+  });
+};
+
+export const saveProfile = (profile) => {
+  return setItem(PROFILE_KEY, profile);
+};
